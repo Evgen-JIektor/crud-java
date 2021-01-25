a26681d9995ad2d9bee3a596fae4803dd74a8ad4
Evgen Krasnoshchekov <4445500@bk.ru>
2021-01-25 19:42:19 +0300

    test commit

---
+++ src/main/java/com/example/javacrud/JavaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrudApplication.class, args);
+	}
+
+}
